365a493fed6f07cd805586b7c556f1fc0054c53a
juliading <jding713@gmail.com>
2021-08-31 18:46:50 -0500

    node server & participant component

---
+++ components/Participant/Participant.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "Participant.module.scss";
+
+export default function Participant() {
+    return (
+          <div className={styles.participantName}>
+            Name
+        </div>
+      );
+  };
